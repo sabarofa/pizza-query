b0b7f1c3c82c5676536fd0a4d2ae60dfdd5fa136
abdm@outlook.com <abdm@outlook.com>
2023-05-26 14:54:10 +0000

    Ola mundo do primeiro dia
    Aqui aprendemos

---
+++ Dia01/OlaMundo.sql
@@ -0,0 +1,7 @@
+-- Databricks notebook source
+Select 'Olá Mundo!'
+
+-- COMMAND ----------
+
+select * -- Selecione todas as colunas 
+from silver.pizza_query.pedido -- da tabela silver.pizza_query.pedido
